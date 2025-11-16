f26f3eb3b267691c6440aefebd75d19fcd6ac96b
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2025-11-16 22:45:49 +0100

    Estructuras basicas - Arrays

---
+++ src/Estructuras_basicas/Uso_ArrayList/UsoArrayList.java
@@ -0,0 +1,7 @@
+package Estructuras_basicas.Uso_ArrayList;
+
+public class UsoArrayList {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Estructuras_basicas/Uso_Arrays/UsoArrays.java
+++ src/Estructuras_basicas/Uso_Arrays/UsoArrays.java
@@ -1,5 +1,5 @@
 package Estructuras_basicas.Uso_Arrays;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class UsoArrays {
@@ -7,5 +7,27 @@ public class UsoArrays {
 
         Scanner sc = new Scanner(System.in);
         int[] numeros = new int[5];
+        int indice = 0;
+
+        for (int i=0; i<numeros.length; i++){
+            System.out.println("Introduce el número " + (i+1) + ": ");
+            numeros[i] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        //Mostrar números introducidos
+        for (int num:numeros){
+            System.out.println("Número: " + num);
+        }
+
+        //Otra opción mostrar numeros:
+        while (indice < numeros.length) {
+            System.out.println("Posicion " + (indice+1) + ": " + numeros[indice]);
+            indice++;
+        }
+
+        //Otra opción mostrar numeros:
+        Arrays.stream(numeros).forEach(num -> System.out.println("Elemento: " + num));
+
     }
 }
